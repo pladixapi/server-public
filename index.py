3d322c27ff7e0284c7d38ff561d889a8c396b774
pladixapi <119880975+pladixapi@users.noreply.github.com>
2022-12-11 19:34:34 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,108 @@
+from typing import Optional
+from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Request
+from fastapi.responses  import RedirectResponse
+from fastapi.responses import JSONResponse
+from bins import mydict, get_bin_info
+import re
+from pydantic import BaseModel
+from py_adyen_encrypt import encryptor
+import urllib
+
+
+def adyen_enc(cc, mes, ano, cvv, ADYEN_KEY, adyen_version):
+    enc = encryptor(ADYEN_KEY)
+    enc.adyen_version = adyen_version
+    enc.adyen_public_key = ADYEN_KEY
+
+    card = enc.encrypt_card(card=cc, cvv=cvv, month=mes, year=ano)
+    month = card['month']
+    year = card['year']
+    cvv = card['cvv']
+    card = card['card']
+
+    card = urllib.parse.quote_plus(card)
+    Month = urllib.parse.quote_plus(month)
+    Year = urllib.parse.quote_plus(year)
+    cvv = urllib.parse.quote_plus(cvv)
+    return card, Month, Year,cvv
+
+
+app = FastAPI(debug=True, 
+              title="Servidor Publico de PladixOficial", 
+              redoc_url=None,
+              description=" Ola meus caros amigos, sejam bem vindos ao servidor publico de PladixOficial.")
+
+
+
+
+class UnicornException(Exception):
+    def __init__(self, name: str,  message: str):
+        self.name = name
+        self.message = message
+
+
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    return JSONResponse(
+        status_code=418,
+        content={'success': False, "message": exc.message},
+    )
+
+
+@app.get("/bin/{bin1}")
+async def bin(bin1):
+    price = bin1
+    if not price.isdigit():
+        raise HTTPException(
+            status_code=404, detail="Digite pelo menos os 6 primeiros digitos do cartao.")
+    elif len(price) < 6:
+        raise HTTPException(
+            status_code=404, detail="Digite pelo menos os 6 primeiros digitos do cartao.")
+    bin = price[:6]
+    bin_data = get_bin_info(bin)
+    if not bin_data:
+        raise HTTPException(status_code=404, detail="BIN nao foi encontrada, tente novamente.")
+    return {
+        'bin': bin,
+        'bank': bin_data['bank_name'],
+        'country_iso': bin_data['iso'],
+        'country': bin_data['country'],
+        'flag': bin_data['flag'],
+        'vendor': bin_data['vendor'],
+        'type': bin_data['type'],
+        'level': bin_data['level'],
+        'prepaid': bin_data['prepaid'],                                                                               
+    }
+
+
+@app.get("/")
+async def start():
+
+    #return RedirectResponse("http://www.github.com/r0ld3x/adyen-enc-and-bin-info")
+    return JSONResponse(
+        status_code=404,
+        content={'success': False, "message": "Nenhum arquivo foi encontrado neste diretorio."},
+    )
+
+
+class Item(BaseModel):
+    card: str
+    month: str
+    year: str
+    cvv: str
+    adyen_version: str
+    adyen_key: str
+
+
+@app.post("/adyen/")
+async def adyen(item: Item):
+    cc, mes, ano, cvv = adyen_enc(
+        item.card, item.month, item.year, item.cvv, item.adyen_key, item.adyen_version)
+    return {
+        'card': cc,
+        'month': mes,
+        'year': ano,
+        'cvv': cvv
+    }
